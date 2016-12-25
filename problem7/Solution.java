84d505ecc799473f3e11263c6aa625ba2dd8c9e1
cutoutsy <cutoutsy@gmail.com>
2016-12-25 18:49:33 +0800

    add Solution.java for problem7

---
+++ problem7/Solution.java
@@ -0,0 +1,15 @@
+public class Solution{
+	public int reverse(int x){
+		StringBuffer sb = new StringBuffer();
+		if(x < 0){
+			sb.append("-");
+		}
+		
+		System.out.println("X:"+x);
+		return 0;
+	}
+	
+	public static void main(String[] args){
+		System.out.println("hello world");
+	}
+}
