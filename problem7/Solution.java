7988a5b400d8b85f1c02777b722bb813a414d51a
cutoutsy <cutoutsy@gmail.com>
2016-12-25 19:20:11 +0800

    completed problem 7

--- problem7/Solution.java
+++ problem7/Solution.java
@@ -1,15 +1,30 @@
 public class Solution{
 	public int reverse(int x){
+		int re = 0;
 		StringBuffer sb = new StringBuffer();
 		if(x < 0){
 			sb.append("-");
+			x = Math.abs(x);
 		}
 		
-		System.out.println("X:"+x);
-		return 0;
+		String stringX = String.valueOf(x);
+		for(int i = stringX.length()-1; i >= 0; i--){
+			sb.append(stringX.charAt(i));
+		}
+		
+		try{
+			re = Integer.valueOf(sb.toString());
+		}catch(Exception ex){
+			re = 0;
+		}
+		return re;
 	}
 	
 	public static void main(String[] args){
-		System.out.println("hello world");
+		Solution su = new Solution();
+		System.out.println(su.reverse(123));
+		System.out.println(su.reverse(-123));
+		System.out.println(su.reverse(100));
+		System.out.println(su.reverse(1000000003));
 	}
 }
