93c38fab963c01c1e59a17daf30577583431a749
cutoutsy <cutoutsy@gmail.com>
2017-05-03 11:20:31 +0800

    complete problem 557

---
+++ problem557/Solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public String reverseWords(String s){
+        StringBuffer sb = new StringBuffer();
+        String[] words = s.split("\\s");
+        for (int i = 0; i < words.length; i++) {
+            for (int j = words[i].length()-1; j >= 0; j--) {
+                sb.append(words[i].charAt(j));
+            }
+            sb.append(" ");
+        }
+        return sb.toString().substring(0, sb.length()-1);
+    }
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        String input = "Let's take LeetCode contest";
+        String result = solution.reverseWords(input);
+        System.out.println(result);
+    }
+}
