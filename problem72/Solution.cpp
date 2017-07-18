b9771cabc3b5d948da57c093ad1c27adfb8c4703
cutoutsy <cutoutsy@gmail.com>
2017-07-18 17:14:05 +0800

    reduce space for problem 72

--- problem72/Solution.cpp
+++ problem72/Solution.cpp
@@ -2,18 +2,22 @@ class Solution {
 public:
     int minDistance(string word1, string word2) {
         int m = word1.length(), n = word2.length();
-        vector<vector<int> > dp(m+1, vector<int>(n+1));
+        vector<int> dp(n+1);
         for (int i = 0; i <= m; ++i) {
+            int last;
             for (int j = 0; j <= n; ++j) {
                 if(i == 0){
-                    dp[i][j] = j;
+                    dp[j] = j;
                 }else if(j == 0){
-                    dp[i][j] = i;
+                    last = dp[j];
+                    dp[j] = i;
                 }else{
-                    dp[i][j] = min(dp[i-1][j-1] + ((word1[i-1] == word2[j-1])?0:1), min(dp[i][j-1]+1, dp[i-1][j]+1));
+                    int temp = dp[j];
+                    dp[j] = min(last + ((word1[i-1] == word2[j-1])?0:1), min(dp[j-1]+1, dp[j]+1));
+                    last = temp;
                 }
             }
         }
-        return dp[m][n];
+        return dp[n];
     }
 };
