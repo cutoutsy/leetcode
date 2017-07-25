7df1891393791e7954bfee85c19554b142dffee7
cutoutsy <cutoutsy@gmail.com>
2017-07-25 22:02:57 +0800

    finish problem 200 with c++

---
+++ problem200/Solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void help(vector<vector<char>>& grid, int i, int j){
+        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[i].size() || grid[i][j] != '1'){
+            return;
+        }
+        grid[i][j] = 0;
+        help(grid, i + 1, j);
+        help(grid, i - 1, j);
+        help(grid, i, j+1);
+        help(grid, i, j-1);
+
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int answer = 0;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[i].size(); ++j) {
+                if(grid[i][j] == '1'){
+                    help(grid, i, j);
+                    ++answer;
+                }
+            }
+        }
+        return answer;
+        }
+};
