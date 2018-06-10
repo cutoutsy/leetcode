8db5597570358b8aeb8761313b20c798517cc1dc
cutoutsy <cutoutsy@gmail.com>
2018-06-10 09:35:05 +0800

    finish problem 62

---
+++ medium/problem62/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[n+1][m+1];
+        dp[0][1] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[n][m];
+    }
+}
